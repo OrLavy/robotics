705d5f83648c674417fc7d40d77fb1deeff4e31b
colman <colman@ubuntu>
2015-06-09 08:17:27 -0700

    Created WayPointManager

---
+++ CornerStoneBProject/src/WayPointsManager.cpp
@@ -0,0 +1,14 @@
+/*
+ * WayPointsManager.cpp
+ *
+ *  Created on: Jun 9, 2015
+ *      Author: colman
+ */
+
+#include "WayPointsManager.h"
+
+WayPointsManager::WayPointsManager() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ CornerStoneBProject/src/WayPointsManager.h
@@ -0,0 +1,16 @@
+/*
+ * WayPointsManager.h
+ *
+ *  Created on: Jun 9, 2015
+ *      Author: colman
+ */
+
+#ifndef WAYPOINTSMANAGER_H_
+#define WAYPOINTSMANAGER_H_
+
+class WayPointsManager {
+public:
+	WayPointsManager();
+};
+
+#endif /* WAYPOINTSMANAGER_H_ */
